d9ef4078237ee3126a35c167ca5c4e156066a193
Casey Kelly <casey.kelly@colorado.edu>
2018-08-02 20:43:42 -0600

    js - reverse-string - test 2

--- javascript/reverse-string/reverse-string.js
+++ javascript/reverse-string/reverse-string.js
@@ -1,5 +1,11 @@
-const reverseString = () => {
-  return '';
+const reverseString = input => {
+  // Split the input into an array containing the string's characters, 
+  // reverse the items in the array, and join them into the reversed string
+  const reversedString = input
+    .split('')
+    .reverse()
+    .join('');
+  return reversedString;
 };
 
 module.exports = reverseString;
