b2047d9e8a0fa8e21c5443cce3f44d35807f2e07
Casey Kelly <casey.kelly@colorado.edu>
2018-08-02 21:09:31 -0600

    js - triangle - test 1 - refactor

--- javascript/triangle/triangle.js
+++ javascript/triangle/triangle.js
@@ -1,6 +1,18 @@
 class Triangle {
+  constructor(sideA, sideB, sideC) {
+    this.sideA = sideA;
+    this.sideB = sideB;
+    this.sideC = sideC;
+  }
+
   kind() {
-    return 'equilateral';
+    return this.checkEqilateral(this.sideA, this.sideB, this.sideC)
+  }
+
+  checkEqilateral(sideA, sideB, sideC) {
+    if ((sideA + sideB + sideC) / 3 === sideA) {
+      return 'equilateral';
+    }
   }
 }
 
